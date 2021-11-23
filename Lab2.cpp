518ef51ab18954288b2fe096dde597285c978245
Violetta <ranreo.reo@gmail.com>
2021-11-23 20:20:45 +0500

    Помещение объектов в переменные различных типов

--- Lab2.cpp
+++ Lab2.cpp
@@ -49,6 +49,50 @@ public:
     void reset();
 };
 
+class ColoredPoint: public Point {
+
+protected:
+
+    int color;
+
+public:
+
+    ColoredPoint() : Point() {
+
+        printf("ColoredPoint()\n");
+
+        color = 0;
+    }
+
+    ColoredPoint(int x, int y, int color) : Point(x, y) {
+
+        printf("ColoredPoint(int x, int y)\n");
+
+        this->color = color;
+    }
+
+    ColoredPoint(const ColoredPoint& p) {
+
+        printf("ColoredPoint(const Point& p)\n");
+
+        color = p.color;
+        x = p.x;
+        y = p.y;
+    }
+
+    ~ColoredPoint() {
+
+        printf("%d, %d color = %d\n", x, y, color);
+
+        printf("~ColoredPoint()\n");
+    }
+
+    void changeColor(int new_color) {
+
+        color = new_color;
+    }
+};
+
 void Point::reset() {
     x = 0;
     y = 0;
@@ -59,6 +103,7 @@ void chapterOne()
     // Работа со статическими и динамическими объектами
     // Их создание и уничтожение
 
+    cout << "PART ONE!\n" << endl;
 
     cout << "Static objects: " << endl;
 
@@ -78,7 +123,7 @@ void chapterOne()
 
     Point* p3 = new Point(*p2);
 
-
+    cout << "\nDeleting" << endl;
     delete p;
     delete p2;
     delete p3;
@@ -89,14 +134,66 @@ void chapterTwo()
     // Реализация методов
     // Доступность атрибутов
 
-    cout << "\nMethods reset() and move(int dx, int dy): " << endl;
+    cout << "\nPART TWO!\n" << endl;
 
     Point* p = new Point(1, 2);
 
     p->reset();
     p->move(10, 10);
     
+    cout << "\nDeleting" << endl;
     delete p;
+}
+
+void chapterThree()
+{
+    // Наследование
+
+    cout << "\nPART THREE!\n" << endl;
+
+    ColoredPoint* p = new ColoredPoint(1, 2, 42);
+
+    cout << "\nDeleting" << endl;
+    delete p;
+}
+
+void chapterFour()
+{
+    // Помещение объектов в переменные
+    // различных типов
+
+    cout << "\nPART FOUR!" << endl;
+
+    // Помещение объекта потомка
+    // в переменную-указатель на базовый класс:
+    // Point* p = new ColoredPoint(1, 2, 42);
+
+    // Невозможно. Нельзя поместить
+    // объект базового класса в переменную- 
+    // указатель на потомка.
+    // ColoredPoint* p = new Point(1, 2, 42);
+
+    // Невозможно. Та же ситуация.
+    // Point* p2 = new Point(1, 2);
+    // ColoredPoint* p3 = p2;
+
+    cout << "\nPoint* p1 = new ColoredPoint(1, 2, 42)" << endl;
+    Point* p1 = new ColoredPoint(1, 2, 42);
+
+    cout << "\nColoredPoint* p2 = new ColoredPoint(1, 2, 42)" << endl;
+    ColoredPoint* p2 = new ColoredPoint(1, 2, 42);
+
+    // Если поместить объект в переменную-указатель
+    // на базовый класс, то будут доступны только
+    // методы базового класса.
+    // Если же в переменную-указатель на
+    // класс потомка, то будут доступны методы
+    // и базового класса, и класса потомка.
+
+    cout << "\nDeleting" << endl;
+
+    delete p1;
+    delete p2;
 }
 
 int main()
@@ -105,8 +202,9 @@ int main()
 
     chapterTwo();
 
+    chapterThree();
 
-
+    chapterFour();
 
     return 0;
 }
